4ff012f4eb8f771840a3f7afa8cd4e88f410f655
Marco Boom <marco@quest-media.nl>
2017-01-27 18:58:11 +0100

    Make module compatible with crud and base module

--- resources/views/backend/fields/media-item.blade.php
+++ resources/views/backend/fields/media-item.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	<label for="{{ $field->id() }}" class="col-sm-3 control-label">{{ $field->label() }}</label>
 	<div class="col-sm-9">
-		<media-item name="{{ $field->id() }}" :item="{!! $field->format($model) ?: 'null' !!}"></media-item>
+		<media-item name="{{ $field->id() }}" :item="{{ $field->format($model) ?: 'null' }}" controller="/media/ajax"></media-item>
 	</div>
 </div>
---
+++ src/Controllers/CrudController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace LaravelAdmin\MediaManager\Controllers;
+
+use Illuminate\Http\Request;
+
+use LaravelAdmin\MediaManager\Models\Media;
+use LaravelAdmin\MediaManager\Upload;
+use App\Http\Controllers\Controller;
+
+use LaravelAdmin\Crud\Traits\CrudController as CrudTrait;
+
+class CrudController extends Controller
+{
+	use CrudTrait {
+        update as traitUpdate;
+    }
+
+	protected $model = Media::class;
+
+	protected $singular_name = 'media';
+	protected $plural_name = 'media';
+
+
+	public function store(Request $request)
+	{
+		//	Delete files
+		if ($request->has('items'))
+		{
+			$trigger = $this->model('deleteMultiple', $request->items);
+
+			return back();
+		}
+
+		//	Upload files
+		if ($file = Upload::handle($request, 'file'))
+		{
+			$this->flash("The file is uploaded");
+
+			return $this->redirect("index");
+		}
+
+		return back();
+	}
+
+
+	public function update(Request $request, $id)
+	{
+		$model = $this->traitUpdate($request, $id, false);
+
+		if ($request->file('replace'))
+		{
+			Upload::update($media)->handle($request, 'replace');
+		}
+
+		$this->flash('The changes has been saved');
+
+		return back();
+	}
+
+	protected function getValidationRulesOnUpdate()
+	{
+		return [
+			'name' => 'required',
+		];
+	}
+
+	public function getFieldsForList()
+	{
+		return [
+			['label'=>'Name', 'formatter'=>'name'],
+			['label'=>'Size', 'formatter'=>'sizeFormatted'],
+			['label'=>'Type', 'formatter'=>'type'],
+			['label'=>'Created', 'formatter'=>function($model)
+			{
+				return $model->created_at->format('Y-m-d');
+			}],
+		];
+	}
+
+	protected function getFieldsForEdit()
+	{
+		return [
+			[
+				'id'		=>	'name',
+				'label'		=>	'Name',
+				'driver'	=>	'text',
+				'formatter'	=>	'name',
+			],
+
+			[
+				'id'		=>	'replace',
+				'label'		=>	'Replace source file',
+				'driver'	=>	'file',
+				'formatter'	=>	null,
+			]
+		];
+	}
+
+
+	protected function getFieldsForCreate()
+	{
+		return [
+			[
+				'id'		=>	'file',
+				'label'		=>	'Upload file',
+				'driver'	=>	'file',
+				'formatter'	=>	null,
+			]
+		];
+	}
+}
