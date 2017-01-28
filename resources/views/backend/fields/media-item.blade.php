1762447e465320a24f035a0e5b0bc76f6e0d76b3
Marco Boom <marco@quest-media.nl>
2017-01-28 17:03:27 +0100

    Set controller of crud media item field by the media route

--- resources/views/backend/fields/media-item.blade.php
+++ resources/views/backend/fields/media-item.blade.php
@@ -1,6 +1,6 @@
 <div class="form-group">
 	<label for="{{ $field->id() }}" class="col-sm-3 control-label">{{ $field->label() }}</label>
 	<div class="col-sm-9">
-		<media-item name="{{ $field->id() }}" :item="{{ $field->format($model) ?: 'null' }}" controller="/media/ajax"></media-item>
+		<media-item name="{{ $field->id() }}" :item="{{ $field->format($model) ?: 'null' }}" controller="{{ route('admin.media-manager.ajax.index') }}"></media-item>
 	</div>
 </div>
