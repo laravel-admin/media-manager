dad826325ba29769226fed073da1e8e2f7c25a0e
Marco Boom <marco@quest-media.nl>
2017-03-03 15:33:52 +0100

    Match base config

--- src/routes.php
+++ src/routes.php
@@ -1,50 +1,38 @@
 <?php
 
-Route::group(['middleware'=>'web'], function()
-{
+Route::group(['middleware'=>'web'], function () {
+    Route::group(config('media.routes.frontend.options'), function () {
+        Route::get('img/{imagestyle}/{id}/{slug?}', config('media.routes.frontend.controller').'@img');
+        Route::get('file/{id}/{slug?}', config('media.routes.frontend.controller').'@file');
+    });
 
-	Route::group(config('media.routes.frontend.options'), function()
-	{
-		Route::get('img/{imagestyle}/{id}/{slug?}', config('media.routes.frontend.controller').'@img');
-		Route::get('file/{id}/{slug?}', config('media.routes.frontend.controller').'@file');
-	});
+    if (class_exists(LaravelAdmin\Base\BaseServiceProvider::class)) {
+        Route::group(config('admin.routeGroup') + ['middleware'=>config('admin.routeMiddleware')], function () {
+            Route::resource('media/ajax', LaravelAdmin\MediaManager\Controllers\AjaxController::class, ['only'=>['index','store'], 'names' => [
+                'index' => config('media.routes.ajax.name').'index',
+                'store' => config('media.routes.ajax.name').'store',
+            ]]);
 
-	if (class_exists(LaravelAdmin\Base\BaseServiceProvider::class))
-	{
-		Route::group(config('admin.route_group') + ['middleware'=>config('admin.route_middleware')], function()
-		{
-			Route::resource('media/ajax', LaravelAdmin\MediaManager\Controllers\AjaxController::class, ['only'=>['index','store'], 'names' => [
-				'index' => config('media.routes.ajax.name').'index',
-				'store' => config('media.routes.ajax.name').'store',
-			]]);
+            Route::resource('media', LaravelAdmin\MediaManager\Controllers\CrudController::class);
+        });
+    } else {
+        Route::group(config('media.routes.ajax.options'), function () {
+            Route::resource('media/ajax', config('media.routes.ajax.controller'), ['only'=>['index','store'], 'names' => [
+                'index' => config('media.routes.ajax.name').'index',
+                'store' => config('media.routes.ajax.name').'store',
+            ]]);
+        });
 
-			Route::resource('media', LaravelAdmin\MediaManager\Controllers\CrudController::class);
-		});
-	}
-
-	else
-	{
-		Route::group(config('media.routes.ajax.options'), function()
-		{
-			Route::resource('media/ajax', config('media.routes.ajax.controller'), ['only'=>['index','store'], 'names' => [
-				'index' => config('media.routes.ajax.name').'index',
-				'store' => config('media.routes.ajax.name').'store',
-			]]);
-		});
-
-		Route::group(config('media.routes.backend.options'), function()
-		{
-			Route::resource('media', config('media.routes.backend.controller'), ['names' => [
-				 'index' => config('media.routes.backend.name').'index',
-				 'create' => config('media.routes.backend.name').'create',
-				 'store' => config('media.routes.backend.name').'store',
-				 'edit' => config('media.routes.backend.name').'edit',
-				 'show' => config('media.routes.backend.name').'show',
-				 'update' => config('media.routes.backend.name').'update',
-				 'destroy' => config('media.routes.backend.name').'destroy',
-			]]);
-		});
-	}
-
-
+        Route::group(config('media.routes.backend.options'), function () {
+            Route::resource('media', config('media.routes.backend.controller'), ['names' => [
+                 'index' => config('media.routes.backend.name').'index',
+                 'create' => config('media.routes.backend.name').'create',
+                 'store' => config('media.routes.backend.name').'store',
+                 'edit' => config('media.routes.backend.name').'edit',
+                 'show' => config('media.routes.backend.name').'show',
+                 'update' => config('media.routes.backend.name').'update',
+                 'destroy' => config('media.routes.backend.name').'destroy',
+            ]]);
+        });
+    }
 });
