065be608e49b1560bb92bab218dab299667afecf
Marco Boom <marco@quest-media.nl>
2017-05-26 14:48:21 +0200

    Make compatible with Crud/Layout

--- src/Controllers/AjaxController.php
+++ src/Controllers/AjaxController.php
@@ -9,38 +9,44 @@ use LaravelAdmin\MediaManager\Upload;
 
 class AjaxController extends Controller
 {
-	public function __construct()
-	{
-		//$this->middleware(config('media.routes.ajax.middleware'));
-	}
+    public function __construct()
+    {
+        //$this->middleware(config('media.routes.ajax.middleware'));
+    }
 
-	/**
-	 * List all media from the database, paginated by 8
-	 * @param  Request $request
-	 * @return Response
-	 */
+    /**
+     * List all media from the database, paginated by 8
+     * @param  Request $request
+     * @return Response
+     */
     public function index(Request $request)
-	{
-		$builder = Media::orderBy('created_at', 'desc');
+    {
+        $builder = Media::orderBy('created_at', 'desc');
 
-		if ($request->has('s')) $builder->where('name','like','%'.$request->s.'%');
+        if ($request->has('s')) {
+            $builder->where('name', 'like', '%'.$request->s.'%');
+        }
 
-		return $builder->paginate(8);
-	}
+        return $builder->paginate(8);
+    }
 
-	/**
-	 * Upload a file from a POST request
-	 * @param  Request $request
-	 * @return Response | null
-	 */
+    /**
+     * Upload a file from a POST request
+     * @param  Request $request
+     * @return Response | null
+     */
 
-	public function store(Request $request)
-	{
-		if ($media = Upload::handle($request, 'file'))
-		{
-			return $media;
-		}
+    public function store(Request $request)
+    {
+        if ($media = Upload::handle($request, 'file')) {
+            return $media;
+        }
 
-		return null;
-	}
+        return null;
+    }
+
+    public function show($id)
+    {
+        return Media::findOrFail($id);
+    }
 }
