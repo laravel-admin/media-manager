9d78b57a35b36e7f01247e7e4048d353c99486c4
Bas de Beer <bas@dekantvandebeer.nl>
2018-07-03 09:28:45 +0200

    Added allowed_filetypes to media config

--- resources/config/media.php
+++ resources/config/media.php
@@ -1,76 +1,74 @@
 <?php
 
 return [
+    'path' => [
+        'root' => 'uploads',
+        'styles' => 'styles',
+        'format' => '%Y/%m/%d',
+    ],
 
-	'path'	=>	[
-		'root'		=>	'uploads',
-		'styles'	=>	'styles',
-		'format'	=>	'%Y/%m/%d',
-	],
+    'upload' => [
+        'default_driver' => 'fileupload',
 
-	'upload'		=>	[
+        'max_filesize' => 6000,
 
-		'default_driver'	=>	'fileupload',
+        'allowed_filetypes' => null,
 
-		'max_filesize' => 6000,
+        'drivers' => [
+                'fileupload' => LaravelAdmin\MediaManager\Drivers\Uploads\FileUpload::class,
+                'url' => LaravelAdmin\MediaManager\Drivers\Uploads\Url::class,
+        ],
+    ],
 
-		'drivers'			=>	[
-				'fileupload' 	=>	LaravelAdmin\MediaManager\Drivers\Uploads\FileUpload::class,
-				'url'			=>	LaravelAdmin\MediaManager\Drivers\Uploads\Url::class,
-		],
-	],
+    'routes' => [
+        'frontend' => [
+            'controller' => LaravelAdmin\MediaManager\Controllers\FrontendController::class,
+            'name' => 'media-manager.frontend.',
+            'options' => [],
+        ],
 
-	'routes'		=>	[
-		'frontend'	=>	[
-			'controller'	=>	LaravelAdmin\MediaManager\Controllers\FrontendController::class,
-			'name'			=>	'media-manager.frontend.',
-			'options'		=>	[],
-		],
+        'backend' => [
+            'controller' => LaravelAdmin\MediaManager\Controllers\BackendController::class,
+            'name' => 'media-manager.backend.',
+            'options' => [
+                'middleware' => ['auth'],
+                'prefix' => 'admin',
+            ],
+        ],
 
-		'backend'	=>	[
-			'controller'	=>	LaravelAdmin\MediaManager\Controllers\BackendController::class,
-			'name'			=>	'media-manager.backend.',
-			'options'		=>	[
-				'middleware'	=>	['auth'],
-				'prefix'		=>	'admin',
-			],
-		],
+        'ajax' => [
+            'controller' => LaravelAdmin\MediaManager\Controllers\AjaxController::class,
+            'name' => 'media-manager.ajax.',
+            'options' => [
+                'middleware' => ['auth'],
+                'prefix' => 'admin',
+            ],
+        ],
+    ],
 
-		'ajax'		=>	[
-			'controller'	=>	LaravelAdmin\MediaManager\Controllers\AjaxController::class,
-			'name'			=>	'media-manager.ajax.',
-			'options'		=>	[
-				'middleware'	=>	['auth'],
-				'prefix'		=>	'admin',
-			],
-		],
-	],
+    'imagestyles' => [
+        'thumbnail' => [
+            'id' => 'thumbnail',
+            'name' => 'Thumbnail (100x100)',
+            'actions' => [
+                LaravelAdmin\MediaManager\Drivers\Responses\GetFromCache::class => [],
+                LaravelAdmin\MediaManager\Drivers\Effects\Crop::class => ['width' => 100, 'height' => 100],
+                LaravelAdmin\MediaManager\Drivers\Responses\SaveToCache::class => [],
+            ]
+        ],
 
-	'imagestyles'	=>	[
+        'header' => [
+            'id' => 'header',
+            'name' => 'Header (1400x500)',
+            'actions' => [
+                LaravelAdmin\MediaManager\Drivers\Effects\Fit::class => ['width' => 1400, 'height' => 500],
+                LaravelAdmin\MediaManager\Drivers\Responses\SaveToStorage::class => [],
+            ]
+        ],
+    ],
 
-		'thumbnail'	=>	[
-			'id'	=>	'thumbnail',
-			'name'	=>	'Thumbnail (100x100)',
-			'actions' => [
-				LaravelAdmin\MediaManager\Drivers\Responses\GetFromCache::class => [],
-				LaravelAdmin\MediaManager\Drivers\Effects\Crop::class => ['width'=>100, 'height'=>100],
-				LaravelAdmin\MediaManager\Drivers\Responses\SaveToCache::class => [],
-			]
-		],
-
-		'header'	=>	[
-			'id'	=>	'header',
-			'name'	=>	'Header (1400x500)',
-			'actions'	=>	[
-				LaravelAdmin\MediaManager\Drivers\Effects\Fit::class => ['width'=>1400, 'height'=>500],
-				LaravelAdmin\MediaManager\Drivers\Responses\SaveToStorage::class => [],
-			]
-		],
-	],
-
-	'views'		=>	[
-			'layout'	=>	'layouts.app',
-			'section'	=>	'content',
-	],
-
+    'views' => [
+            'layout' => 'layouts.app',
+            'section' => 'content',
+    ],
 ];
