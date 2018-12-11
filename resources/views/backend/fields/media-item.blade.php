29c0d1546e6987ffbad89f467c3684b370555ac0
Bas de Beer <bas@dekantvandebeer.nl>
2018-12-11 12:24:47 +0100

    Fix error when filetypes is null

--- resources/views/backend/fields/media-item.blade.php
+++ resources/views/backend/fields/media-item.blade.php
@@ -7,7 +7,7 @@
             $model->$current_id = old($field->id());
             @endphp
         @endif
-        <media-item name="{{ $field->id() }}" filetypes="{{ $field->filetypes() ?: 'null' }}" :item="{{ $field->format($model) ?: 'null' }}" controller="{{ route('admin.media-manager.ajax.index') }}"></media-item>
+        <media-item name="{{ $field->id() }}" filetypes="{{ $field->filetypes() }}" :item="{{ $field->format($model) ?: 'null' }}" controller="{{ route('admin.media-manager.ajax.index') }}"></media-item>
         @if($field->description())
             <p style="padding-top:6px;">{{ $field->description() }}</p>
         @endif
